0a0a37093ea67ef84ab086424756de92a98f5713
Fedor Ptitsyn <fedor.ptith@gmail.com>
2026-02-22 22:39:18 +0300

    Found view by ID

--- practice1/layouttype/src/main/java/ru/mirea/ptitsynfa/layouttype/MainActivity.java
+++ practice1/layouttype/src/main/java/ru/mirea/ptitsynfa/layouttype/MainActivity.java
@@ -1,6 +1,9 @@
 package ru.mirea.ptitsynfa.layouttype;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.table);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+
+            TextView tv = findViewById(R.id.textView1);
+            tv.setText("New text in Mirea");
+
+            Button button = findViewById(R.id.button1);
+            button.setText("Crazy button");
+
+            CheckBox checkBox = findViewById(R.id.checkBox1);
+            checkBox.setChecked(true);
+
             return insets;
         });
     }

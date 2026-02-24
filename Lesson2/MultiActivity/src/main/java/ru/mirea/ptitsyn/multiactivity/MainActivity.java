fb1a84eef0fda5e86841b335d0aba0bde510161f
Fedor Ptitsyn <fedor.ptith@gmail.com>
2026-02-24 11:59:08 +0300

    Started lesson 2

---
+++ Lesson2/MultiActivity/src/main/java/ru/mirea/ptitsyn/multiactivity/MainActivity.java
@@ -0,0 +1,42 @@
+package ru.mirea.ptitsyn.multiactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button sendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            editText = findViewById(R.id.editTextText);
+            sendButton = findViewById(R.id.sendButton);
+
+            return insets;
+        });
+    }
+
+    public void onClickNewActivity(View view) {
+        String userInput = editText.getText().toString();
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("userInput", userInput);
+        startActivity(intent);
+    }
+}

56cde5e641e8297e4914e3045014ce50c9b511de
Fedor Ptitsyn <fedor.ptith@gmail.com>
2026-03-12 01:17:22 +0300

    Started lesson 3
    First module (IntentApp)

---
+++ Lesson3/IntentApp/src/main/java/ru/mirea/ptitsyn/Lesson3/SecondActivity.java
@@ -0,0 +1,33 @@
+package ru.mirea.ptitsyn.Lesson3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+        String date = intent.getStringExtra("date");
+
+        TextView textView = findViewById(R.id.textView);
+        String message = String.format("КВАДРАТ ЗНАЧЕНИЯ МОЕГО НОМЕРА ПО СПИСКУ СОСТАВЛЯЕТ 400, а текущее время %s", date);
+        textView.setText(message);
+    }
+}
